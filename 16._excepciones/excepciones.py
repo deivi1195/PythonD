55ff7a3345c1ac9d163d287580641480665e12a8
deivi1195 <deivig1195@gmail.com>
2023-02-28 11:01:54 -0400

    manejo de try, except

---
+++ 16._excepciones/excepciones.py
@@ -0,0 +1,45 @@
+#creando funcion que suma numeros
+def sumar_dos():
+    #iniciando un bucle
+    while True:
+        #pidiendo numeros    
+        a = input("numero 1: ")
+        b = input("numero 2: ")
+        #intentando convertirlos a entero y sumarlos
+        try:
+            resultado = int(a) + int(b)
+        #si lanzo una excepcion, pedirle que reingrese los datos
+        except:
+            print("no escribiste un numero, vuelve a intentar escribir un numero")
+        #si todo salio bien terminamos el bucle
+        else:
+            break
+        finally:
+            print("manejo de excepcion finalizado")
+            
+    #mostrando el resultado        
+    return resultado
+
+print(sumar_dos())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
