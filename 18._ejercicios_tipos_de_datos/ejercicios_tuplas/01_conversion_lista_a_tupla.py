ef59f5a34624ac35c7add09ed0cd96fccc114b2d
deivi1195 <deivig1195@gmail.com>
2023-03-01 16:03:52 -0400

    invertir tupla

--- 18._ejercicios_tipos_de_datos/ejercicios_tuplas/01_conversion_lista_a_tupla.py
+++ 18._ejercicios_tipos_de_datos/ejercicios_tuplas/01_conversion_lista_a_tupla.py
@@ -7,25 +7,3 @@ lenguajes_a_tupla = tuple(lenguajes)
 print(lenguajes_a_tupla)
 print(type(lenguajes_a_tupla))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ 18._ejercicios_tipos_de_datos/ejercicios_tuplas/02_invertir_tupla.py
@@ -0,0 +1,8 @@
+# Invertir el contenido de una tupla
+
+numeros = (1,2,3,4,5,6,7,8,9,10)
+
+print(numeros[::-1])
+
+
+
