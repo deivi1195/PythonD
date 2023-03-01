42f408a51e0317e4017b97ef68d665e26cc9f5a2
deivi1195 <deivig1195@gmail.com>
2023-03-01 15:43:23 -0400

    ejercicio lista a tupla

---
+++ 18._ejercicios_tipos_de_datos/ejercicios_tuplas/conversion_lista_a_tupla.py
@@ -0,0 +1,31 @@
+#convertir una lista en una tupla
+
+lenguajes = ['Python', 'JavaScript', 'PHP', 'C#', 'C++', 'Java']
+
+lenguajes_a_tupla = tuple(lenguajes)
+
+print(lenguajes_a_tupla)
+print(type(lenguajes_a_tupla))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
