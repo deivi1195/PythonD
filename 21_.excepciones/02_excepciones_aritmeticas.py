7fec2d3d79db315900b79c627b18c69050b4199e
deivi1195 <deivig1195@gmail.com>
2023-03-06 12:04:11 -0400

    Gestion de excepciones en operaciones artimeticas - division

---
+++ 21_.excepciones/02_excepciones_aritmeticas.py
@@ -0,0 +1,37 @@
+# Gestion de excepciones en operaciones artimeticas - division:
+print('Gestion de excepciones en operaciones artimeticas - division:')
+
+# Captura del primer numero - dividendo
+
+while True:
+    try:
+        dividendo = float(input('Escriba el dividendo: '))
+        
+        break
+    except:
+        print("MENSAJE: debe escribir un valor valido. Intente nuevamente.\n")
+
+while True:
+    try:
+        divisor = float(input('Escriba el divisor: '))
+        
+        break
+    except:
+        print("MENSAJE: debe escribir un valor valido. Intente nuevamente.\n")    
+        
+division = dividendo / divisor
+
+print(f'El resultado de la division es: {division}')
+
+
+
+
+
+
+
+
+
+
+
+
+
